d23974b2c105060851002962a0964f912ad1fa04
evandro.limeira <evandro.limeira@itadvanced.com.br>
2025-04-08 12:13:06 -0300

    pegando alguma coisa

--- memory/memory.c
+++ memory/memory.c
@@ -1,66 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/sysinfo.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
-#ifdef _WIN32
-    #include <windows.h>
-#else
-    #include <sys/mman.h>
-    #include <unistd.h>
-#endif
 
-void* memo_map(size_t tamanho) {
+void* memo_map(size_t lenght) {
     void* ptr;
-    
-#ifdef _WIN32
-    // Windows: VirtualAlloc
-    ptr = VirtualAlloc(NULL, tamanho, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-    if (ptr == NULL) {
-        printf("Erro ao alocar memória no Windows: %d\n", GetLastError());
-        return NULL;
-    }
-#else
     // Linux/macOS/Unix: usage mmap
-    ptr = mmap(NULL, tamanho, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    ptr = mmap(NULL, lenght, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
     if (ptr == MAP_FAILED) {
-        perror("Erro ao mapear memória no Linux/macOS");
+        perror("Erro to try map memory on Linux/macOS");
         return NULL;
     }
-#endif
-    
+
     return ptr;
 }
 
-void free_memo(void* ptr, size_t tamanho) {
-#ifdef _WIN32
-    VirtualFree(ptr, 0, MEM_RELEASE);
-#else
-    munmap(ptr, tamanho);
-#endif
+void free_memo(int* ptr, size_t lenght) {
+    munmap(ptr, lenght);
+}
+
+size_t memo_lenght() {
+    struct sysinfo info;
+
+    if (sysinfo(&info) == 0) {
+        size_t total = (size_t)info.totalram * info.mem_unit / (1024 * 1024);
+        return total;
+    }
 }
 
 int main() {
-    size_t tamanho_memoria = 4096;
-    char* memoria_mapeada;
-    char* ponteiro;
-    
-    memoria_mapeada = (char*)memo_map(tamanho_memoria);
-    if (memoria_mapeada == NULL) {
-        return 1;
-    }
-    
-    printf("Memória mapeada em: %p\n", (void*)memoria_mapeada);
-    
-    ponteiro = memoria_mapeada;
-    const char* mensagem = "Teste de escrita na memória mapeada!";
-    strcpy(ponteiro, mensagem);
-    
-    printf("Conteúdo escrito: %s\n", ponteiro);
-    
-    ponteiro += 6;
-    printf("Conteúdo a partir do offset 6: %s\n", ponteiro);
-    
-    free_memo(memoria_mapeada, tamanho_memoria);
-    
-    return 0;
+    size_t lenght = memo_lenght();
+    void* ptr = memo_map(lenght);
+
+    printf("-----------------------------------------------------------------------------------------\n");
+    printf("/                                   CHEAT MEMORY                                        /\n");
+    printf("-----------------------------------------------------------------------------------------\n");
+
+    memo_map(lenght);
+    free_memo(ptr, lenght);
 }
